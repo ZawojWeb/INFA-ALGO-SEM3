39d99e89adb6e912de5816075e55c19481d9b42e
ZawojWeb <79516803+ZawojWeb@users.noreply.github.com>
2021-11-25 23:54:52 +0100

    Asis L4

--- Asis/lab04/zad1.sh
+++ Asis/lab04/zad1.sh
@@ -1,3 +1,4 @@
+# Kacper Zawojski 261692
 #!/bin/bash
 export LC_NUMERIC="en_US.UTF-8"
 
--- Asis/lab04/zad2.sh
+++ Asis/lab04/zad2.sh
@@ -1,24 +1,5 @@
-# for PID in $(ls -l /proc | awk '{print $9}' | grep -Eo '[0-9]{1,9}' | sort -n );
-#             do
-
-#             TTY=$(cat /proc/$PID/stat 2>/dev/null | awk '{print $7}')
-#             STAT=$(cat /proc/$PID/stat 2>/dev/null | awk '{print $3}')
-#             UTIME=$(cat /proc/$PID/stat 2>/dev/null | awk '{print $14}')
-#             STIME=$(cat /proc/$PID/stat 2>/dev/null | awk '{print $17}')
-
-#             #Delete NULL byte error:
-#             CMD=$(cat /proc/$PID/cmdline | tr -d '\0' < /proc/$PID/cmdline)
-
-#             CLK_TCK=$(getconf CLK_TCK)
-#             TTIME=$((UTIME + STIME))
-#             TIME=$((TTIME / CLK_TCK))
-
-#             # Fix line too long error:
-#             if [ -n "$CMD" ]; then
-#                     printf "%-8s | %-15s | %s\n" "$PID | $TTY | $STAT | $TIME | $CMD" | column -t  -s '|'
-#             fi
-#         done
-
+# Kacper Zawojski 261692
+#!/bin/bash
 for PID in $(ls -l /proc | awk '{print $9}' | grep -Eo '[0-9]{1,9}' | sort -n );
     do
         #  PRINT USER
