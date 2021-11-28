3a24950c341dfaab51ba093dbbacd095687a291a
ZawojWeb <79516803+ZawojWeb@users.noreply.github.com>
2021-11-28 15:21:59 +0100

    Update zad4.sh

--- Asis/lab04/zad4.sh
+++ Asis/lab04/zad4.sh
@@ -8,7 +8,7 @@ URL=$1
 for (( ; ; )); do
     mv new.html old.html 2> /dev/null
     curl $URL -L --compressed -s > new.html
-    DIFF_OUTPUT="$(diff new.html old.html)"
+    DIFF_OUTPUT="$(diff new.html old.html | tee logs.txt )"
     if [ "0" != "${#DIFF_OUTPUT}" ]; then
         xmessage "Changes are avilable"
         sleep $2
