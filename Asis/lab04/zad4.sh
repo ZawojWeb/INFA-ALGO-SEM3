b47cb684af23aa9948a3016ef0d5decd7fd081b4
ZawojWeb <79516803+ZawojWeb@users.noreply.github.com>
2021-11-28 13:20:44 +0100

    Update zad4.sh

--- Asis/lab04/zad4.sh
+++ Asis/lab04/zad4.sh
@@ -3,7 +3,7 @@
 # monitor.sh - Monitors a web page for changes
 # sends an email notification if the file change
 
-URL="https://zawojweb.com/"
+URL=$1
 
 for (( ; ; )); do
     mv new.html old.html 2> /dev/null
@@ -11,6 +11,6 @@ for (( ; ; )); do
     DIFF_OUTPUT="$(diff new.html old.html)"
     if [ "0" != "${#DIFF_OUTPUT}" ]; then
         xmessage "Changes are avilable"
-        sleep 1
+        sleep $2
     fi
 done
