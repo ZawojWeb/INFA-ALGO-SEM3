1c7019becc1fe29be9b4c2759ef66d21d0cfbda5
ZawojWeb <79516803+ZawojWeb@users.noreply.github.com>
2021-11-28 13:12:42 +0100

    zad4.sh

---
+++ Asis/lab04/zad4.sh
@@ -0,0 +1,16 @@
+ #!/bin/bash
+
+# monitor.sh - Monitors a web page for changes
+# sends an email notification if the file change
+
+URL="https://zawojweb.com/"
+
+for (( ; ; )); do
+    mv new.html old.html 2> /dev/null
+    curl $URL -L --compressed -s > new.html
+    DIFF_OUTPUT="$(diff new.html old.html)"
+    if [ "0" != "${#DIFF_OUTPUT}" ]; then
+        xmessage "Changes are avilable"
+        sleep 1
+    fi
+done
