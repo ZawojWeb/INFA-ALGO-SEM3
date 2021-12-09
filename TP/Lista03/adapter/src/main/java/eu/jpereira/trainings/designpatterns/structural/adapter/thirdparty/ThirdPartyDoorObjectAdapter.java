aafd4832db76a50accb0093f328f608f03a60b5e
Zawoj <79516803+ZawojWeb@users.noreply.github.com>
2021-12-09 10:39:58 +0100

    1.3.5

--- TP/Lista03/adapter/src/main/java/eu/jpereira/trainings/designpatterns/structural/adapter/thirdparty/ThirdPartyDoorObjectAdapter.java
+++ TP/Lista03/adapter/src/main/java/eu/jpereira/trainings/designpatterns/structural/adapter/thirdparty/ThirdPartyDoorObjectAdapter.java
@@ -1,40 +1,95 @@
 package eu.jpereira.trainings.designpatterns.structural.adapter.thirdparty;
 
 import eu.jpereira.trainings.designpatterns.structural.adapter.exceptions.CodeMismatchException;
+// import eu.jpereira.trainings.designpatterns.structural.adapter.exceptions.CodeMismatchException;
 import eu.jpereira.trainings.designpatterns.structural.adapter.exceptions.IncorrectDoorCodeException;
 import eu.jpereira.trainings.designpatterns.structural.adapter.model.Door;
+import eu.jpereira.trainings.designpatterns.structural.adapter.thirdparty.ThirdPartyDoor.DoorState;
+import eu.jpereira.trainings.designpatterns.structural.adapter.thirdparty.ThirdPartyDoor.LockStatus;
+import eu.jpereira.trainings.designpatterns.structural.adapter.thirdparty.exceptions.CannotChangeCodeForUnlockedDoor;
+import eu.jpereira.trainings.designpatterns.structural.adapter.thirdparty.exceptions.CannotChangeStateOfLockedDoor;
+import eu.jpereira.trainings.designpatterns.structural.adapter.thirdparty.exceptions.CannotUnlockDoorException;
 
 public class ThirdPartyDoorObjectAdapter implements Door {
 
-    @Override
-    public void open(String code) throws IncorrectDoorCodeException {
-        // TODO Auto-generated method stub
-        
-    }
+	ThirdPartyDoor door = new ThirdPartyDoor();
 
-    @Override
-    public void close() {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void open(String code) throws IncorrectDoorCodeException {
+		// TODO Auto-generated method stub
+		if (!isOpen()) {
+			try {
+				door.unlock(code);
+				if (door.getLockStatus().equals(LockStatus.UNLOCKED)) {
+					door.setState(DoorState.OPEN);
+				}
 
-    @Override
-    public boolean isOpen() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+			} catch (CannotUnlockDoorException e) {
+				// TODO Auto-generated catch block
+				throw new IncorrectDoorCodeException();
+			} catch (CannotChangeStateOfLockedDoor e) {
+				// TODO Auto-generated catch block
+				throw new IncorrectDoorCodeException();
+			}
+		}
+	}
 
-    @Override
-    public void changeCode(String oldCode, String newCode, String newCodeConfirmation)
-            throws IncorrectDoorCodeException, CodeMismatchException {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void close() {
+		try {
+			if (isOpen()) {
+				// TODO Auto-generated method stub
+				door.setState(DoorState.CLOSED);
+			}
+		} catch (CannotChangeStateOfLockedDoor e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public boolean testCode(String code) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-    
+	@Override
+	public boolean isOpen() {
+		// TODO Auto-generated method stub
+		if (door.getState().equals(DoorState.OPEN)) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void changeCode(String oldCode, String newCode, String newCodeConfirmation) throws IncorrectDoorCodeException,
+			CodeMismatchException {
+		// TODO Auto-generated method stub
+		try {
+			if (door.getLockStatus().equals(LockStatus.LOCKED)) {
+				door.unlock(oldCode);
+			}
+			if (!newCode.equals(newCodeConfirmation)) {
+				door.lock();
+			}
+
+			door.setNewLockCode(newCode);
+		} catch (CannotChangeCodeForUnlockedDoor e) {
+			// TODO Auto-generated catch block
+			throw new CodeMismatchException();
+		} catch (CannotUnlockDoorException e) {
+			// TODO Auto-generated catch block
+			throw new IncorrectDoorCodeException();
+		}
+
+	}
+
+	@Override
+	public boolean testCode(String code) {
+		// TODO Auto-generated method stub
+		try {
+			door.setNewLockCode(code);
+			return true;
+		} catch (CannotChangeCodeForUnlockedDoor e) {
+			// TODO Auto-generated catch block
+			return false;
+		}
+	}
+
 }
+
