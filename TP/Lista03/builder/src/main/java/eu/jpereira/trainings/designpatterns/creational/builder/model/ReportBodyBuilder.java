de6832add9f04f3d0759ad06263db6475b77a6be
Kacper Zawojski <zawojweb@gmail.com>
2021-12-10 16:50:37 +0100

    Builder Complete

--- TP/Lista03/builder/src/main/java/eu/jpereira/trainings/designpatterns/creational/builder/model/ReportBodyBuilder.java
+++ TP/Lista03/builder/src/main/java/eu/jpereira/trainings/designpatterns/creational/builder/model/ReportBodyBuilder.java
@@ -1,5 +1,10 @@
 package eu.jpereira.trainings.designpatterns.creational.builder.model;
 
-public class ReportBodyBuilder {
-    
+public interface ReportBodyBuilder {
+
+    ReportBodyBuilder setCustomerName(String customerName);
+    ReportBodyBuilder setCustomerPhone(String phoneNumber);
+    ReportBodyBuilder withItems();
+    ReportBodyBuilder newItem(String name, int quantity, double price);
+    ReportBody getReportBody();
 }
